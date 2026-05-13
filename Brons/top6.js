d532a53e90c2e457c58249f467aa92c497650dd1
RohRohRaggy <Max.nikoliszyn@student.gildeopleidingen.nl>
2026-05-13 13:00:32 +0200

    top6songs

---
+++ Brons/top6.js
@@ -0,0 +1,20 @@
+// Opdracht 1
+const top6 = [
+    "This Place Is Built (Sugar Hill Mix)", 
+    "Berreta (De Los Cerros La Escuela)", 
+    "Feel Good Inc.", "Groove District", 
+    "Água de Beber", 
+    "Everything Stays (featuring Olivia Olson)",
+    "Tani VHS"
+];
+
+// Opdracht 2
+console.log(top6[0]);
+
+// Opdracht 3
+console.log(top6[2]);
+
+// Opdracht 4
+top6.forEach(function(song) {
+    console.log(song);
+});
